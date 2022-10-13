ead0e608d2fe761b2aef4a3a7bfbd81fece06f8d
kookwangs <82722851+kookwangs@users.noreply.github.com>
2022-10-13 19:48:18 +0700

    add shutdown

--- kunanya_code/game_page_shutdown.py
+++ kunanya_code/game_page_shutdown.py
@@ -35,7 +35,7 @@ pygame.display.set_icon(dog)
 screen.blit(sushi,(138,495))
 screen.blit(food,(1047,495))
 
-pygame.mixer.music.load(musicPath+"/jingle_bell_music.mp3")
+pygame.mixer.music.load(musicPath+"/shutdown_music.mp3")
 
 catX = 497
 dogX = 666
@@ -43,18 +43,12 @@ cloudX = 0
 cloudBack = 0
 
 scoreValue = 0
-
-countSFX = 1
-global countPlaySFX
 countPlaySFX = 0
-recentSoundTime = 1000000000
-beatType = "c"
 
 
 array.array("i")                
 a = array.array("i",(0 for i in range(0,36)))
 a[0] = 1
-b = array.array("i",(0 for i in range(0,2)))
 
 def _showText(text,x,y):
     showText=font.render(text,True,(0,0,0))
@@ -151,110 +145,111 @@ while True:
     presentTicks=(pygame.time.get_ticks()-startTicks)/1000 
     
     if a[1] == 0:       
-        countPlaySFX += _callCat(1,4.40,countPlaySFX)
-        scoreValue += _checkCat(1,4.40,key)
+        countPlaySFX += _callDog(1,3.2,countPlaySFX)
+        scoreValue += _checkDog(1,3.2,key)
     elif a[2] == 0:
-        countPlaySFX += _callDog(2,9.1,countPlaySFX)
-        scoreValue += _checkDog(2,9.1,key)
+        countPlaySFX += _callCat(2,6.5,countPlaySFX)
+        scoreValue += _checkCat(2,6.5,key)
     elif a[3] == 0:
-        countPlaySFX += _callCat(3,11.3,countPlaySFX)
-        scoreValue += _checkCat(3,11.3,key)
+        countPlaySFX += _callDog(3,9.8,countPlaySFX)
+        scoreValue += _checkDog(3,9.8,key) 
     elif a[4] == 0:
-        countPlaySFX += _callDog(4,13.70,countPlaySFX)
-        scoreValue += _checkDog(4,13.70,key)
+        countPlaySFX += _callCat(4,13.1,countPlaySFX)
+        scoreValue += _checkCat(4,13.1,key)
     elif a[5] == 0:
-        countPlaySFX += _callDog(5,16,countPlaySFX)
-        scoreValue += _checkDog(5,16,key)
+        countPlaySFX += _callDog(5,16.32,countPlaySFX)
+        scoreValue += _checkDog(5,16.32,key)
     elif a[6] == 0:
-        countPlaySFX += _callCat(6,18.3,countPlaySFX)
-        scoreValue += _checkCat(6,18.3,key)
+        countPlaySFX += _callDog(6,17.9,countPlaySFX)
+        scoreValue += _checkDog(6,17.9,key)
     elif a[7] == 0:
-        countPlaySFX += _callDog(7,22.9,countPlaySFX)
-        scoreValue += _checkDog(7,22.9,key)
+        countPlaySFX += _callCat(7,19.6,countPlaySFX)
+        scoreValue += _checkCat(7,19.6,key)#now
     elif a[8] == 0:
-        countPlaySFX += _callCat(8,25.1,countPlaySFX)
-        scoreValue += _checkCat(8,25.1,key)
+        countPlaySFX += _callCat(8,21.2,countPlaySFX)
+        scoreValue += _checkCat(8,21.2,key)#now
     elif a[9] == 0:
-        countPlaySFX += _callDog(9,28.8,countPlaySFX)
-        scoreValue += _checkDog(9,28.8,key)
+        countPlaySFX += _callDog(9,24.5,countPlaySFX)
+        scoreValue += _checkDog(9,24.5,key)
     elif a[10] == 0:
-        countPlaySFX += _callDog(10,31.1,countPlaySFX)
-        scoreValue += _checkDog(10,31.1,key)
+        countPlaySFX += _callCat(10,26.14,countPlaySFX)
+        scoreValue += _checkCat(10,26.14,key)#now
     elif a[11] == 0:
-        countPlaySFX += _callCat(11,37.9,countPlaySFX)
-        scoreValue += _checkCat(11,37.9,key)
+        countPlaySFX += _callDog(11,29.4,countPlaySFX)
+        scoreValue += _checkDog(11,29.4,key)#now
     elif a[12] == 0:
-        countPlaySFX += _callCat(12,38.9,countPlaySFX)
-        scoreValue += _checkCat(12,38.9,key)
+        countPlaySFX += _callCat(12,31,countPlaySFX)
+        scoreValue += _checkCat(12,31,key)
     elif a[13] == 0:
-        countPlaySFX += _callDog(13,41.5,countPlaySFX)
-        scoreValue += _checkDog(13,41.5,key)
+        countPlaySFX += _callDog(13,32.6,countPlaySFX)
+        scoreValue += _checkDog(13,32.6,key)#now
     elif a[14] == 0:
-        countPlaySFX += _callDog(14,46.1,countPlaySFX)
-        scoreValue += _checkDog(14,46.1,key)
+        countPlaySFX += _callCat(14,34.3,countPlaySFX)
+        scoreValue += _checkCat(14,34.3,key)#now
     elif a[15] == 0:
-        countPlaySFX += _callDog(15,47.3,countPlaySFX)
-        scoreValue += _checkDog(15,47.3,key)
+        countPlaySFX += _callDog(15,37.65,countPlaySFX)
+        scoreValue += _checkDog(15,37.65,key)
     elif a[16] == 0:
-        countPlaySFX += _callDog(16,48.5,countPlaySFX)
-        scoreValue += _checkDog(16,48.5,key)
+        countPlaySFX += _callDog(16,39.25,countPlaySFX)
+        scoreValue += _checkDog(16,39.25,key)
     elif a[17] == 0:
-        countPlaySFX += _callCat(17,51.1,countPlaySFX)
-        scoreValue += _checkCat(17,51.1,key)
+        countPlaySFX += _callCat(17,42.55,countPlaySFX)
+        scoreValue += _checkCat(17,42.55,key)
     elif a[18] == 0:
-        countPlaySFX += _callCat(18,52.9,countPlaySFX)
-        scoreValue += _checkCat(18,52.9,key)
+        countPlaySFX += _callCat(18,44.2,countPlaySFX)
+        scoreValue += _checkCat(18,44.2,key)
     elif a[19] == 0:
-        countPlaySFX += _callCat(19,55.2,countPlaySFX)
-        scoreValue += _checkCat(19,55.2,key)
+        countPlaySFX += _callCat(19,47.45,countPlaySFX)
+        scoreValue += _checkCat(19,47.45,key)
     elif a[20] == 0:
-        countPlaySFX += _callDog(20,57.6,countPlaySFX)
-        scoreValue += _checkDog(20,57.6,key)
+        countPlaySFX += _callDog(20,50.74,countPlaySFX)
+        scoreValue += _checkDog(20,50.74,key)
     elif a[21] == 0:
-        countPlaySFX += _callCat(21,59.8,countPlaySFX)
-        scoreValue += _checkCat(21,59.8,key)
+        countPlaySFX += _callCat(21,52.3,countPlaySFX)
+        scoreValue += _checkCat(21,52.3,key)
+        #bf hook
     elif a[22] == 0:
-        countPlaySFX += _callDog(22,64.56,countPlaySFX)
-        scoreValue += _checkDog(22,64.56,key)
+        countPlaySFX += _callDog(22,54.7,countPlaySFX)
+        scoreValue += _checkDog(22,54.7,key)
     elif a[23] == 0:
-        countPlaySFX += _callCat(23,66.8,countPlaySFX)
-        scoreValue += _checkCat(23,66.8,key)
+        countPlaySFX += _callCat(23,57.23,countPlaySFX)
+        scoreValue += _checkCat(23,57.23,key)
     elif a[24] == 0:
-        countPlaySFX += _callDog(24,70.35,countPlaySFX)
-        scoreValue += _checkDog(24,70.35,key)
+        countPlaySFX += _callDog(24,58.9,countPlaySFX)
+        scoreValue += _checkDog(24,58.9,key)
     elif a[25] == 0:
-        countPlaySFX += _callDog(25,71.4,countPlaySFX)
-        scoreValue += _checkDog(25,71.4,key)
+        countPlaySFX += _callDog(25,60.5,countPlaySFX)
+        scoreValue += _checkDog(25,60.5,key)
     elif a[26] == 0:
-        countPlaySFX += _callDog(26,72.6,countPlaySFX)
-        scoreValue += _checkDog(26,72.6,key)
+        countPlaySFX += _callDog(26,62.2,countPlaySFX)
+        scoreValue += _checkDog(26,62.2,key)
     elif a[27] == 0:
-        countPlaySFX += _callCat(27,73.8,countPlaySFX)
-        scoreValue += _checkCat(27,73.8,key)
+        countPlaySFX += _callCat(27,63.7,countPlaySFX)
+        scoreValue += _checkCat(27,63.7,key)
     elif a[28] == 0:
-        countPlaySFX += _callCat(28,76,countPlaySFX)
-        scoreValue += _checkCat(28,76,key)
+        countPlaySFX += _callCat(28,67,countPlaySFX)
+        scoreValue += _checkCat(28,67,key)
     elif a[29] == 0:
-        countPlaySFX += _callDog(29,78.5,countPlaySFX)
-        scoreValue += _checkDog(29,78.5,key)
+        countPlaySFX += _callDog(29,68.6,countPlaySFX)
+        scoreValue += _checkDog(29,68.6,key)
     elif a[30] == 0:
-        countPlaySFX += _callCat(30,82.9,countPlaySFX)
-        scoreValue += _checkCat(30,82.9,key)
+        countPlaySFX += _callCat(30,70.26,countPlaySFX)
+        scoreValue += _checkCat(30,70.26,key)
     elif a[31] == 0:
-        countPlaySFX += _callCat(31,84.2,countPlaySFX)
-        scoreValue += _checkCat(31,84.2,key)
+        countPlaySFX += _callCat(31,71.98,countPlaySFX)
+        scoreValue += _checkCat(31,71.98,key)
     elif a[32] == 0:
-        countPlaySFX += _callDog(32,85.5,countPlaySFX)
-        scoreValue += _checkDog(32,85.5,key)
+        countPlaySFX += _callDog(32,73.56,countPlaySFX)
+        scoreValue += _checkDog(32,73.56,key)
     elif a[33] == 0:
-        countPlaySFX += _callDog(33,86.6,countPlaySFX)
-        scoreValue += _checkDog(33,86.6,key)
+        countPlaySFX += _callDog(33,75.2,countPlaySFX)
+        scoreValue += _checkDog(33,75.2,key)
     elif a[34] == 0:
-        countPlaySFX += _callCat(34,90,countPlaySFX)
-        scoreValue += _checkCat(34,90,key)
+        countPlaySFX += _callCat(34,76.7,countPlaySFX)
+        scoreValue += _checkCat(34,76.7,key)
     elif a[35] == 0:
-        countPlaySFX += _callCat(35,94,countPlaySFX)
-        scoreValue += _checkCat(35,94,key)
+        countPlaySFX += _callCat(35,80.2,countPlaySFX)
+        scoreValue += _checkCat(35,80.2,key)
     
     
     
@@ -267,7 +262,7 @@ while True:
     
     _showCat(catX,400)
     _showDog(dogX,400)
-    _showText("Jingle Bell",562,50)
+    _showText("Shutdown",562,50)
     _showCloud(cloudX,111)
     if cloudX<=100 and cloudBack==0:
         cloudX+=0.5
