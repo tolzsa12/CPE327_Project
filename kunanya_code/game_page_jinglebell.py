2ee9853196b562a3b7f751982a8b3ce0d7d8a130
kookwangs <82722851+kookwangs@users.noreply.github.com>
2022-10-27 13:35:50 +0700

    jingle bell edit

---
+++ kunanya_code/game_page_jinglebell.py
@@ -0,0 +1,300 @@
+import pygame
+import time
+import array
+import os
+
+
+os.chdir("../")
+mainPath = os.getcwd()
+#parentPath = os.path.dirname(os.getcwd())
+#os.chdir(parentPath)
+#parentPath = os.getcwd()
+
+gamePagePath = mainPath + "/olavan_asset/game_page"
+fontPath = mainPath + "/font"
+musicPath = mainPath + "/music"
+highestScorePath = mainPath + "/highest_score"
+
+pygame.init()
+pygame.display.set_caption("gameV.0.1")
+
+screen = pygame.display.set_mode((1280, 800))
+bg=pygame.image.load(gamePagePath+"/game_page.png")
+dog = pygame.image.load(gamePagePath+"/dog.png")
+cat = pygame.image.load(gamePagePath+"/cat.png")
+sushi = pygame.image.load(gamePagePath+"/sushi.png")
+food = pygame.image.load(gamePagePath+"/food.png")
+cloud = pygame.image.load(gamePagePath+"/cloud.png")
+
+hitSound = pygame.mixer.Sound(musicPath+"/hit_sound.mp3")
+missSound = pygame.mixer.Sound(musicPath+"/miss_sound.mp3")
+dogSound = pygame.mixer.Sound(musicPath+"/dog_sound.mp3")
+catSound = pygame.mixer.Sound(musicPath+"/cat_sound.mp3")
+
+hitSound.set_volume(0.4)
+missSound.set_volume(0.4)
+font=pygame.font.Font(fontPath+"/trebuc.ttf",32)
+pygame.display.set_icon(dog)
+
+def _getHighestScore():
+    with open(highestScorePath+"/highest_score_jinglebell.txt","r") as f:
+        return f.read()
+
+def _showText(text,x,y):
+    showText=font.render(text,True,(0,0,0))
+    screen.blit(showText, (x,y))
+
+def _showCat(x,y):
+    screen.blit(cat,(x,y))
+
+def _showDog(x,y):
+    screen.blit(dog,(x,y))
+
+def _showCloud(x,y):
+    screen.blit(cloud,(x,y))
+
+#def testText(x):
+        #text=font.render(str(x),True,(0,0,0))
+        #screen.blit(text, (400,300))
+
+def _callCat(n,t,countPlaySFX,presentTicks):
+    count = 0
+    if presentTicks >= t and countPlaySFX<n:
+        catSound.play()
+        count+=1   
+    return count
+
+
+def _callDog(n,t,countPlaySFX,presentTicks):
+    count = 0
+    if presentTicks >= t and countPlaySFX<n:
+        dogSound.play()
+        count+=1   
+    return count
+
+def _checkCat(countPlaySFX,recentSoundTime,key,presentTicks,a):
+    score = 0
+    if presentTicks>=recentSoundTime and presentTicks<=recentSoundTime+1 and a[countPlaySFX]==0:
+        if key == "f":
+            score+=50
+            hitSound.play()
+            a[countPlaySFX]=1
+        elif key == "j":
+            missSound.play()
+            a[countPlaySFX]=1
+    elif presentTicks>recentSoundTime+1 and a[countPlaySFX]==0:
+            missSound.play()
+            a[countPlaySFX]=1
+    elif a[countPlaySFX]==0 and a[countPlaySFX-1]==1 and (key=="f" or key=="j"):
+            missSound.play()
+    return score
+
+def _checkDog(countPlaySFX,recentSoundTime,key,presentTicks,a):
+    score = 0
+    if presentTicks>=recentSoundTime and presentTicks<=recentSoundTime+1 and a[countPlaySFX]==0:
+        if key == "j":
+            score+=50
+            hitSound.play()
+            a[countPlaySFX]=1
+        elif key == "f":
+            missSound.play()
+            a[countPlaySFX]=1
+    elif presentTicks>recentSoundTime+1 and a[countPlaySFX]==0:
+            missSound.play()
+            a[countPlaySFX]=1
+    elif a[countPlaySFX]==0 and a[countPlaySFX-1]==1 and (key=="f" or key=="j"):
+            missSound.play()
+    return score
+
+
+
+def _play2():
+    #array for counting the beat
+    array.array("i")                
+    a = array.array("i",(0 for i in range(0,36)))
+    a[0] = 1
+    #any variables
+    pygame.mixer.music.load(musicPath+"/jingle_bell_music.mp3")
+    pauseTime = 0
+    pygame.mixer.music.set_volume(0.3)
+    pygame.mixer.music.play(1,0.0)
+    startTicks=pygame.time.get_ticks()
+    countPlaySFX = 0
+    scoreValue = 0
+    cloudX = 0
+    cloudBack = 0
+
+    try:
+        highestScore = int(_getHighestScore())
+    except:
+        highestScore = 0
+    
+    while True:
+        key = "initial"
+        ysushi = 495
+        yfood = 495
+        catX = 497
+        dogX = 666
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_f:            
+                        catX = 255
+                        key = "f"
+                        ysushi = 488
+                    elif event.key == pygame.K_j: 
+                        dogX = 915
+                        key = "j"
+                        yfood = 488
+                    elif event.key == pygame.K_SPACE:
+                        pygame.mixer.music.pause()
+                        from pause_temp import _pauseTime
+                        pauseTemp = _pauseTime()
+                        if pauseTemp == -1:
+                            _play2()
+                        pauseTime += pauseTemp
+                        pygame.mixer.music.unpause()
+                        
+                        
+        presentTicks=(pygame.time.get_ticks()-startTicks-pauseTime)/1000 
+        
+        if a[1] == 0:       
+            countPlaySFX += _callCat(1,4.40,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(1,4.40,key,presentTicks,a)
+        elif a[2] == 0:
+            countPlaySFX += _callDog(2,9.1,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(2,9.1,key,presentTicks,a)
+        elif a[3] == 0:
+            countPlaySFX += _callCat(3,11.3,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(3,11.3,key,presentTicks,a)
+        elif a[4] == 0:
+            countPlaySFX += _callDog(4,13.70,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(4,13.70,key,presentTicks,a)
+        elif a[5] == 0:
+            countPlaySFX += _callDog(5,16,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(5,16,key,presentTicks,a)
+        elif a[6] == 0:
+            countPlaySFX += _callCat(6,18.3,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(6,18.3,key,presentTicks,a)
+        elif a[7] == 0:
+            countPlaySFX += _callDog(7,22.9,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(7,22.9,key,presentTicks,a)
+        elif a[8] == 0:
+            countPlaySFX += _callCat(8,25.1,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(8,25.1,key,presentTicks,a)
+        elif a[9] == 0:
+            countPlaySFX += _callDog(9,28.8,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(9,28.8,key,presentTicks,a)
+        elif a[10] == 0:
+            countPlaySFX += _callDog(10,31.1,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(10,31.1,key,presentTicks,a)
+        elif a[11] == 0:
+            countPlaySFX += _callCat(11,37.9,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(11,37.9,key,presentTicks,a)
+        elif a[12] == 0:
+            countPlaySFX += _callCat(12,38.9,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(12,38.9,key,presentTicks,a)
+        elif a[13] == 0:
+            countPlaySFX += _callDog(13,41.5,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(13,41.5,key,presentTicks,a)
+        elif a[14] == 0:
+            countPlaySFX += _callDog(14,46.1,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(14,46.1,key,presentTicks,a)
+        elif a[15] == 0:
+            countPlaySFX += _callDog(15,47.3,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(15,47.3,key,presentTicks,a)
+        elif a[16] == 0:
+            countPlaySFX += _callDog(16,48.5,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(16,48.5,key,presentTicks,a)
+        elif a[17] == 0:
+            countPlaySFX += _callCat(17,51.1,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(17,51.1,key,presentTicks,a)
+        elif a[18] == 0:
+            countPlaySFX += _callCat(18,52.9,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(18,52.9,key,presentTicks,a)
+        elif a[19] == 0:
+            countPlaySFX += _callCat(19,55.2,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(19,55.2,key,presentTicks,a)
+        elif a[20] == 0:
+            countPlaySFX += _callDog(20,57.6,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(20,57.6,key,presentTicks,a)
+        elif a[21] == 0:
+            countPlaySFX += _callCat(21,59.8,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(21,59.8,key,presentTicks,a)
+        elif a[22] == 0:
+            countPlaySFX += _callDog(22,64.56,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(22,64.56,key,presentTicks,a)
+        elif a[23] == 0:
+            countPlaySFX += _callCat(23,66.8,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(23,66.8,key,presentTicks,a)
+        elif a[24] == 0:
+            countPlaySFX += _callDog(24,70.35,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(24,70.35,key,presentTicks,a)
+        elif a[25] == 0:
+            countPlaySFX += _callDog(25,71.4,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(25,71.4,key,presentTicks,a)
+        elif a[26] == 0:
+            countPlaySFX += _callDog(26,72.6,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(26,72.6,key,presentTicks,a)
+        elif a[27] == 0:
+            countPlaySFX += _callCat(27,73.8,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(27,73.8,key,presentTicks,a)
+        elif a[28] == 0:
+            countPlaySFX += _callCat(28,76,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(28,76,key,presentTicks,a)
+        elif a[29] == 0:
+            countPlaySFX += _callDog(29,78.5,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(29,78.5,key,presentTicks,a)
+        elif a[30] == 0:
+            countPlaySFX += _callCat(30,82.9,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(30,82.9,key,presentTicks,a)
+        elif a[31] == 0:
+            countPlaySFX += _callCat(31,84.2,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(31,84.2,key,presentTicks,a)
+        elif a[32] == 0:
+            countPlaySFX += _callDog(32,85.5,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(32,85.5,key,presentTicks,a)
+        elif a[33] == 0:
+            countPlaySFX += _callDog(33,86.6,countPlaySFX,presentTicks)
+            scoreValue += _checkDog(33,86.6,key,presentTicks,a)
+        elif a[34] == 0:
+            countPlaySFX += _callCat(34,90,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(34,90,key,presentTicks,a)
+        elif a[35] == 0:
+            countPlaySFX += _callCat(35,94,countPlaySFX,presentTicks)
+            scoreValue += _checkCat(35,94,key,presentTicks,a)
+        
+        #preferences
+        screen.blit(bg,(0,0))
+        _showCat(catX,400)
+        _showDog(dogX,400)
+        _showText("Jingle Bell",562,50)
+        _showCloud(cloudX,111)
+        if cloudX<=100 and cloudBack==0:
+            cloudX+=0.5
+            if cloudX==100:
+                cloudBack = 1
+        if cloudBack==1 and cloudX>=-100:
+            cloudX-=0.5
+            if cloudX==-100:
+                cloudBack = 0
+            
+        _showText("Score: "+str(scoreValue),554,730)
+        
+        pygame.time.delay(10)
+        catX = 497
+        dogX = 666
+        
+        screen.blit(sushi,(138,ysushi))
+        screen.blit(food,(1047,yfood))
+        pygame.display.update()
+
+        if presentTicks >= 15:
+            if scoreValue > highestScore:
+                highestScore = scoreValue
+                print(highestScore)
+            with open(highestScorePath+"/highest_score_jinglebell.txt","w") as f:
+                f.write(str(highestScore))
+            
+_play2()
